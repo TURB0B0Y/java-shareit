58e21a83eadb648c2902984a416fa9f45015860d
TURB0B0Y <stopmosk0@gmail.com>
2024-01-29 16:04:43 +0300

    2

--- src/main/java/ru/practicum/shareit/exception/APIBadRequestException.java
+++
@@ -1,9 +0,0 @@
-package ru.practicum.shareit.exception;
-
-public class APIBadRequestException extends APIException {
-
-    public APIBadRequestException(String message, Object... args) {
-        super(message, args);
-    }
-
-}
--- src/main/java/ru/practicum/shareit/exception/APIException.java
+++
@@ -1,9 +0,0 @@
-package ru.practicum.shareit.exception;
-
-public class APIException extends RuntimeException {
-
-    public APIException(String message, Object... args) {
-        super(String.format(message, args));
-    }
-
-}
