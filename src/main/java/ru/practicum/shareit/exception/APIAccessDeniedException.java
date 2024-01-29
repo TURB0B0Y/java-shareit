aa84eb4a2b782d16337a86ee66c64e362711f9da
TURB0B0Y <stopmosk0@gmail.com>
2024-01-29 16:36:34 +0300

    6

--- src/main/java/ru/practicum/shareit/exception/APIAccessDeniedException.java
+++
@@ -1,9 +0,0 @@
-package ru.practicum.shareit.exception;
-
-public class APIAccessDeniedException extends APIException {
-
-    public APIAccessDeniedException(String message, Object... args) {
-        super(message, args);
-    }
-
-}
--- src/main/java/ru/practicum/shareit/exception/APINotFoundException.java
+++
@@ -1,9 +0,0 @@
-package ru.practicum.shareit.exception;
-
-public class APINotFoundException extends APIException {
-
-    public APINotFoundException(String message, Object... args) {
-        super(message, args);
-    }
-
-}
